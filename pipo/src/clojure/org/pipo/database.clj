dca46b9e3fbf2e457b48cb5d95cd63b820f46924
Tuukka Koistinen <tvkoisti@gmail.com>
2015-12-10 21:40:40 +0200

    split punching functions

--- pipo/src/clojure/org/pipo/database.clj
+++ pipo/src/clojure/org/pipo/database.clj
@@ -21,9 +21,15 @@
 
 (defn pipo-db [] (db/get-database (get-db-helper) :write))
 
-(defn add-punch [^org.joda.time.DateTime unix-time]
-  (db/insert (pipo-db) :hours {:type "in"
+(defn add-punch [type-str ^org.joda.time.DateTime unix-time]
+  (db/insert (pipo-db) :hours {:type type-str
                                :time (c/to-epoch unix-time)}))
+
+(defn punch-in [^org.joda.time.DateTime unix-time]
+  (add-punch "in" unix-time))
+
+(defn punch-out [^org.joda.time.DateTime unix-time]
+  (add-punch "out" unix-time))
 
 (defn get-punches [where-clause-str]
   (if (instance? String where-clause-str)
@@ -31,10 +37,9 @@
 
 ; (get-punches)
 ; (db/query-seq (pipo-db) :hours {:start [:or 555 (c/to-epoch(t/date-time 2012 3 4))]})
-; (c/to-long (t/now))
-; (c/to-long (l/local-now))
 ; (c/to-epoch (t/date-time 1998 4 25 12 12 12))
-; (t/now)
 ; (f/show-formatters)
 ; (add-punch (t/date-time 2015 3 4))
+; (punch-in (t/date-time 2014 4 5))
+; (punch-out (t/date-time 2015 4 5))
 ; (db/query-seq (pipo-db) :hours "time >= 555 AND type = 'in'")
