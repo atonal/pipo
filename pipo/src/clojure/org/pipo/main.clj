96182e56b94b357aec2546c139159346e997b9dc
Tuukka Koistinen <tvkoisti@gmail.com>
2016-01-11 20:09:19 +0200

    separate prefs to own file

--- pipo/src/clojure/org/pipo/main.clj
+++ pipo/src/clojure/org/pipo/main.clj
@@ -4,12 +4,12 @@
               [neko.threading :refer [on-ui]]
               [neko.find-view :refer [find-view]]
               [neko.ui :refer [config make-ui]]
-              [neko.data.shared-prefs :refer [defpreferences]]
               [neko.log :as log]
               [neko.notify :refer [toast]]
               [neko.dialog.alert :refer [alert-dialog-builder]]
               [clj-time.local :as l]
               [clj-time.coerce :as c]
+              [org.pipo.prefs :as prefs]
               [org.pipo.database :as db]
               [org.pipo.utils :as utils]
               [org.pipo.location :as location])
@@ -23,38 +23,13 @@
 (def ^:const TEXT_SERVICE_STOP "Stop")
 (def ^:const TEXT_WIPE "wipe")
 (def ^:const TEXT_SET_GPS "Set GPS")
-(def ^:const STATE_IN "IN")
-(def ^:const STATE_OUT "OUT")
 (def ^:const WEEK_DIALOG_ID 0)
 (def ^:const GPS_DIALOG_ID 1)
 (def ^:const EXTRA_DATE "org.pipo.EXTRA_DATE")
 (def ^:const RADIUS_M 100)
 (def ^:const THRESHOLD_M 20)
+
 (def location-atom (atom {:lat "" :long ""}))
-
-(defpreferences pipo-prefs "pipo_sp")
-
-; TODO: get the key from the pref-name
-(defmacro defpref [pref-name pref-key default]
-  `(def ~(vary-meta pref-name assoc :tag `:const) {:key ~pref-key :default ~default}))
-
-(defpref PREF_STATE :state STATE_OUT)
-(defpref PREF_YEAR :year 2015)
-(defpref PREF_WEEK :week 51)
-(defpref PREF_DEST_LAT :lat 0)
-(defpref PREF_DEST_LONG :long 0)
-
-(defn pref-set-named [pref-atom pref-name new-val]
-  (swap! pref-atom assoc (:key pref-name) new-val))
-
-(defn pref-set [pref-name new-val]
-  (pref-set-named pipo-prefs pref-name new-val))
-
-(defn pref-get [pref-name & pref-state]
-  (let [pref (or (first pref-state) @pipo-prefs)]
-    (or
-      ((:key pref-name) pref)
-      (:default pref-name))))
 
 (defn set-text [ctx elmt s]
   (on-ui (config (find-view ctx elmt) :text s)))
@@ -82,11 +57,11 @@
   (let [type-latest (db/get-type (db/get-latest-punch))]
     (if (= type-latest db/IN)
       (do
-        (pref-set PREF_STATE STATE_IN)
+        (prefs/pref-set prefs/PREF_STATE prefs/STATE_IN)
         (on-ui (config (find-view ctx ::punch-in-bt) :enabled false))
         (on-ui (config (find-view ctx ::punch-out-bt) :enabled true)))
       (do
-        (pref-set PREF_STATE STATE_OUT)
+        (prefs/pref-set prefs/PREF_STATE prefs/STATE_OUT)
         (on-ui (config (find-view ctx ::punch-in-bt) :enabled true))
         (on-ui (config (find-view ctx ::punch-out-bt) :enabled false))))))
 
@@ -96,24 +71,24 @@
           longitude (.getLongitude ^android.location.Location location)
           dest-location (android.location.Location. "pipo")]
       (set-location latitude longitude)
-      (.setLatitude dest-location (pref-get PREF_DEST_LAT))
-      (.setLongitude dest-location (pref-get PREF_DEST_LONG))
+      (.setLatitude dest-location (prefs/pref-get prefs/PREF_DEST_LAT))
+      (.setLongitude dest-location (prefs/pref-get prefs/PREF_DEST_LONG))
       (let [distance (.distanceTo location dest-location)]
         (on-ui (toast (str "distance: " distance) :short))
-        (cond (and (= (pref-get PREF_STATE) STATE_OUT)
+        (cond (and (= (prefs/pref-get prefs/PREF_STATE) prefs/STATE_OUT)
                    (< distance RADIUS_M))
               (do
                 (db/punch-in-gps (l/local-now))
                 (on-ui (toast "GPS punch in" :short))
                 (update-state ctx))
-              (and (= (pref-get PREF_STATE) STATE_IN)
+              (and (= (prefs/pref-get prefs/PREF_STATE) prefs/STATE_IN)
                    (> distance (+ RADIUS_M THRESHOLD_M)))
               (do
                 (db/punch-out-gps (l/local-now))
                 (on-ui (toast "GPS punch out" :short))
                 (update-state ctx))
               :else
-              (log/w (str "no GPS punch, state: " (pref-get PREF_STATE) ", distance: " distance))
+              (log/w (str "no GPS punch, state: " (prefs/pref-get prefs/PREF_STATE) ", distance: " distance))
               )))))
 
 (defn get-day-color [date]
@@ -169,8 +144,8 @@
                          :background-color (get-day-color date)
                          }]
             ])
-         (let [year (pref-get PREF_YEAR)
-               week (pref-get PREF_WEEK)]
+         (let [year (prefs/pref-get prefs/PREF_YEAR)
+               week (prefs/pref-get prefs/PREF_WEEK)]
            (utils/week-from-week-number week year)))))
 
 (defn update-week-list [ctx]
@@ -178,8 +153,8 @@
   (on-ui (.addView (find-view ctx ::inner-week) (make-ui ctx (make-week-list ctx)))))
 
 (defn update-week-nr-view [ctx new-state]
-  (let [new-year (pref-get PREF_YEAR new-state)
-        new-week (pref-get PREF_WEEK new-state)]
+  (let [new-year (prefs/pref-get prefs/PREF_YEAR new-state)
+        new-week (prefs/pref-get prefs/PREF_WEEK new-state)]
   (set-text ctx ::year-tv (str new-year " / " new-week))
   (on-ui
     (config (find-view ctx ::year-tv)
@@ -187,7 +162,7 @@
             (get-week-color new-year new-week)))))
 
 (defn create-watchers [ctx]
-  (add-watch pipo-prefs :year-week-watcher
+  (add-watch (prefs/get-prefs) :year-week-watcher
              (fn [key atom old-state new-state]
                (update-week-nr-view ctx new-state)
                (update-week-list ctx)))
@@ -212,8 +187,8 @@
 
 (defn change-to-current-week []
   (let [current (utils/get-current-week)]
-    (pref-set PREF_YEAR (:year current))
-    (pref-set PREF_WEEK (:week current))))
+    (prefs/pref-set prefs/PREF_YEAR (:year current))
+    (prefs/pref-set prefs/PREF_WEEK (:week current))))
 
 (declare service-start)
 (defn service-stop [ctx service]
@@ -244,13 +219,13 @@
               :layout-width :wrap
               :layout-height :wrap
               :text "prev"
-              :on-click (fn [_] (let [year (pref-get PREF_YEAR)
-                                      week (pref-get PREF_WEEK)
+              :on-click (fn [_] (let [year (prefs/pref-get prefs/PREF_YEAR)
+                                      week (prefs/pref-get prefs/PREF_WEEK)
                                       previous-week (utils/get-previous-week
                                                       week
                                                       year)]
-                                  (pref-set PREF_YEAR (:year previous-week))
-                                  (pref-set PREF_WEEK (:week previous-week))))}]
+                                  (prefs/pref-set prefs/PREF_YEAR (:year previous-week))
+                                  (prefs/pref-set prefs/PREF_WEEK (:week previous-week))))}]
     [:linear-layout {:id ::middle-layout
                      :orientation :horizontal
                      :layout-width [0 :dp]
@@ -270,27 +245,27 @@
                    :padding-right [20 :px]
                    :gravity :center_vertical
                    :background-color (get-week-color
-                                       (pref-get PREF_YEAR)
-                                       (pref-get PREF_WEEK))
+                                       (prefs/pref-get prefs/PREF_YEAR)
+                                       (prefs/pref-get prefs/PREF_WEEK))
                    :on-click (fn [_] (change-to-current-week))
                    :on-long-click (fn [_] (do
                                             (on-ui
                                               (.showDialog ctx WEEK_DIALOG_ID))
                                             true))
-                   :text (str (pref-get PREF_YEAR) " / " (pref-get PREF_WEEK))}]
+                   :text (str (prefs/pref-get prefs/PREF_YEAR) " / " (prefs/pref-get prefs/PREF_WEEK))}]
       ]
      ]
     [:button {:id ::next-bt
               :layout-width :wrap
               :layout-height :wrap
               :text "next"
-              :on-click (fn [_] (let [year (pref-get PREF_YEAR)
-                                      week (pref-get PREF_WEEK)
+              :on-click (fn [_] (let [year (prefs/pref-get prefs/PREF_YEAR)
+                                      week (prefs/pref-get prefs/PREF_WEEK)
                                       next-week (utils/get-next-week
                                                   week
                                                   year)]
-                                  (pref-set PREF_YEAR (:year next-week))
-                                  (pref-set PREF_WEEK (:week next-week))))}]
+                                  (prefs/pref-set prefs/PREF_YEAR (:year next-week))
+                                  (prefs/pref-set prefs/PREF_WEEK (:week next-week))))}]
     ]
    [:linear-layout {:id ::week-layout
                     :orientation :vertical
@@ -358,7 +333,7 @@
                   :layout-gravity :right
                   :input-type :number
                   :hint "Year"
-                  :text (str (pref-get PREF_YEAR))
+                  :text (str (prefs/pref-get prefs/PREF_YEAR))
                   }
       ]
      [:edit-text {:id ::week-et
@@ -367,7 +342,7 @@
                   :layout-gravity :right
                   :input-type :number
                   :hint "Week"
-                  :text (str (pref-get PREF_WEEK))
+                  :text (str (prefs/pref-get prefs/PREF_WEEK))
                   }
       ]]))
 
@@ -388,7 +363,7 @@
                                       InputType/TYPE_NUMBER_FLAG_DECIMAL
                                       InputType/TYPE_NUMBER_FLAG_SIGNED)
                   :hint "latitude"
-                  :text (str (pref-get PREF_DEST_LAT))
+                  :text (str (prefs/pref-get prefs/PREF_DEST_LAT))
                   }
       ]
      [:edit-text {:id ::long-et
@@ -399,7 +374,7 @@
                                       InputType/TYPE_NUMBER_FLAG_DECIMAL
                                       InputType/TYPE_NUMBER_FLAG_SIGNED)
                   :hint "longitude"
-                  :text (str (pref-get PREF_DEST_LONG))
+                  :text (str (prefs/pref-get prefs/PREF_DEST_LONG))
                   }
       ]]))
 
@@ -432,8 +407,8 @@
                                              (get-text dialog-layout ::week-et))]
                                   (if (week-input-valid? year week)
                                     (do
-                                      (pref-set PREF_YEAR year)
-                                      (pref-set PREF_WEEK week)))))
+                                      (prefs/pref-set prefs/PREF_YEAR year)
+                                      (prefs/pref-set prefs/PREF_WEEK week)))))
            :negative-text "Cancel"
            :negative-callback (fn [_ _] ())})
         (.setView dialog-layout)
@@ -451,8 +426,8 @@
                                                  (get-text dialog-layout ::lat-et))
                                       longitude (read-string
                                                   (get-text dialog-layout ::long-et))]
-                                  (pref-set PREF_DEST_LAT latitude)
-                                  (pref-set PREF_DEST_LONG longitude)))
+                                  (prefs/pref-set prefs/PREF_DEST_LAT latitude)
+                                  (prefs/pref-set prefs/PREF_DEST_LONG longitude)))
            :negative-text "Cancel"
            :negative-callback (fn [_ _] ())})
         (.setView dialog-layout)
---
+++ pipo/src/clojure/org/pipo/prefs.clj
@@ -0,0 +1,33 @@
+(ns org.pipo.prefs
+  (:require [neko.data.shared-prefs :refer [defpreferences]]))
+
+(def ^:const STATE_IN "IN")
+(def ^:const STATE_OUT "OUT")
+
+(defpreferences pipo-prefs "pipo_sp")
+
+; TODO: get the key from the pref-name
+(defmacro defpref [pref-name pref-key default]
+  `(def ~(vary-meta pref-name assoc :tag `:const) {:key ~pref-key :default ~default}))
+
+(defpref PREF_STATE :state STATE_OUT)
+(defpref PREF_YEAR :year 2015)
+(defpref PREF_WEEK :week 51)
+(defpref PREF_DEST_LAT :lat 0)
+(defpref PREF_DEST_LONG :long 0)
+
+(defn get-prefs []
+  pipo-prefs)
+
+(defn pref-set-named [pref-atom pref-name new-val]
+  (swap! pref-atom assoc (:key pref-name) new-val))
+
+(defn pref-set [pref-name new-val]
+  (pref-set-named pipo-prefs pref-name new-val))
+
+(defn pref-get [pref-name & pref-state]
+  (let [pref (or (first pref-state) @pipo-prefs)]
+    (or
+      ((:key pref-name) pref)
+      (:default pref-name))))
+
