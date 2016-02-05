2b72ec2d03020474179e1d097a452ba02a1b2123
Tuukka Koistinen <tvkoisti@gmail.com>
2016-02-05 16:59:12 +0200

    debug splash activity

--- pipo/src/java/org/pipo/SplashActivity.java
+++ pipo/src/java/org/pipo/SplashActivity.java
@@ -7,6 +7,9 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+import android.util.DisplayMetrics;
 import neko.App;
 
 import org.pipo.R;
@@ -18,6 +21,10 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        Log.i("pipo", "dpi: " + metrics.densityDpi);
 
         if (firstLaunch) {
             firstLaunch = false;
@@ -34,9 +41,17 @@ public class SplashActivity extends Activity {
     }
 
     public void setupSplash() {
+        Log.i("pipo", "splashscreen id: " + R.layout.splashscreen);
         setContentView(R.layout.splashscreen);
 
         TextView appNameView = (TextView)findViewById(R.id.splash_app_name);
+        if (appNameView == null) {
+            Toast.makeText(this, "appNameView == null", Toast.LENGTH_SHORT).show();
+            Log.i("pipo", "appNameView == null");
+        } else {
+            Toast.makeText(this, "appNameView != null", Toast.LENGTH_SHORT).show();
+            Log.i("pipo", "appNameView != null");
+        }
         appNameView.setText(R.string.app_name);
 
         Animation rotation = AnimationUtils.loadAnimation(this, R.anim.splash_rotation);
