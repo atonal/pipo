c81b0b362923d59cd8b8c6f3db912e71cdbcae0d
Tuukka Koistinen <tvkoisti@gmail.com>
2016-02-16 18:32:26 +0200

    use single history atom

--- pipo/src/clojure/org/pipo/service.clj
+++ pipo/src/clojure/org/pipo/service.clj
@@ -32,6 +32,7 @@
 (def ^:const HISTORY_LEN 3)
 (def tick-receiver (atom nil))
 (def update-count (atom 0))
+(def history (atom nil))
 
 (defn every-minute? [^org.joda.time.LocalTime local-time]
   true)
@@ -87,45 +88,43 @@
   (swap! history #(take HISTORY_LEN (conj % latest))))
 
 ;; TODO: split this into parts
-(defn make-my-on-location-fn []
-  (let [history (atom nil)]
-    (fn [^android.location.Location location]
-      (let [latitude (.getLatitude ^android.location.Location location)
-            longitude (.getLongitude ^android.location.Location location)
-            dest-location (android.location.Location. "pipo")]
-        (log/d "on-location thread id " (Thread/currentThread))
-        (.setLatitude dest-location (prefs/pref-get prefs/PREF_DEST_LAT))
-        (.setLongitude dest-location (prefs/pref-get prefs/PREF_DEST_LONG))
-        (let [distance (.distanceTo location dest-location)]
-          (log/d (str "distance: " distance))
-          (if (< distance RADIUS_M)
-            (update-history prefs/STATE_IN history)
-            (update-history prefs/STATE_OUT history))
-          (log/d "history-threshold?" @history)
-          (cond (and (= (prefs/pref-get prefs/PREF_STATE) prefs/STATE_OUT)
-                     (< distance RADIUS_M))
-                (do
-                  (if (history-threshold? @history)
-                    ;; stop location updates?
-                    (if (db/punch-in-gps (l/local-now))
-                      (do
-                        (on-ui (toast "GPS punch in" :short))
-                        (prefs/update-state)))))
-                (and (= (prefs/pref-get prefs/PREF_STATE) prefs/STATE_IN)
-                     (> distance (+ RADIUS_M THRESHOLD_M)))
-                (do
-                  (if (history-threshold? @history)
-                    ;; stop location updates?
-                    (if (db/punch-out-gps (l/local-now))
-                      (do
-                        (on-ui (toast "GPS punch out" :short))
-                        (prefs/update-state)))))
-                :else
-                (log/w (str "no GPS punch, state: " (prefs/pref-get prefs/PREF_STATE) ", distance: " distance))
-                ))
-        (if (max-updates)
-          (location/stop-location-updates))
-        ))))
+(defn my-on-location-fn [^android.location.Location location]
+  (let [latitude (.getLatitude ^android.location.Location location)
+        longitude (.getLongitude ^android.location.Location location)
+        dest-location (android.location.Location. "pipo")]
+    (log/d "on-location thread id " (Thread/currentThread))
+    (.setLatitude dest-location (prefs/pref-get prefs/PREF_DEST_LAT))
+    (.setLongitude dest-location (prefs/pref-get prefs/PREF_DEST_LONG))
+    (let [distance (.distanceTo location dest-location)]
+      (log/d (str "distance: " distance))
+      (if (< distance RADIUS_M)
+        (update-history prefs/STATE_IN history)
+        (update-history prefs/STATE_OUT history))
+      (log/d "history-threshold?" @history)
+      (cond (and (= (prefs/pref-get prefs/PREF_STATE) prefs/STATE_OUT)
+                 (< distance RADIUS_M))
+            (do
+              (if (history-threshold? @history)
+                ;; stop location updates?
+                (if (db/punch-in-gps (l/local-now))
+                  (do
+                    (on-ui (toast "GPS punch in" :short))
+                    (prefs/update-state)))))
+            (and (= (prefs/pref-get prefs/PREF_STATE) prefs/STATE_IN)
+                 (> distance (+ RADIUS_M THRESHOLD_M)))
+            (do
+              (if (history-threshold? @history)
+                ;; stop location updates?
+                (if (db/punch-out-gps (l/local-now))
+                  (do
+                    (on-ui (toast "GPS punch out" :short))
+                    (prefs/update-state)))))
+            :else
+            (log/w (str "no GPS punch, state: " (prefs/pref-get prefs/PREF_STATE) ", distance: " distance))
+            ))
+    (if (max-updates)
+      (location/stop-location-updates))
+    ))
 
 (defn time-to-get-location [^org.joda.time.DateTime date-time]
   (let [now (utils/get-local-time date-time)]
@@ -146,7 +145,7 @@
     (if (time-to-get-location now)
       (do
         (log/d "yes")
-        (location/start-location-updates (make-my-on-location-fn))
+        (location/start-location-updates my-on-location-fn)
         (log/d "location updates started"))
       (log/d "no"))))
 
@@ -195,7 +194,7 @@
     ; (set! (.-arg1 msg) start-id)
     ; (.sendMessage service-handler msg)
 
-    (location/start-location-updates (make-my-on-location-fn) (.getLooper service-handler))
+    (location/start-location-updates my-on-location-fn (.getLooper service-handler))
 
     ; TICKs are handled in tick-func by the service-handler thread
     (reset! tick-receiver
